005fc5a9f9215ef472ba41296f8a54946bdb809b
Tellakula Mohan Sai <tellakula.mohan28sai@gmail.com>
2023-02-15 09:46:01 +0530

    changes in hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins from PES1UG20CS468-Tellakula Mohan Sai\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
